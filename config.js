3199a4f0bfb59af513b7b804423d9c372ccd3689
Jimi Flowers <82945790+jimiflowers@users.noreply.gihub.com>
2022-01-11 21:38:48 +0100

    First release

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  nodeWS: 'wss://gladios.aresprotocol.io',
+  denom: 'ARES',
+  decimalPlaces: 12,
+  validator: 'VALIDATOR1_STASH_ACCOUNT_ADDRESS',
+  password: 'YOUR_KEYSTORE_ACCOUNT_PASSWORD',
+  accountJSON: './keystores/ACCOUNT_FILE.json', //JSON ACCOUNT FILE EXPORTED FROM PolkadotJS Extension
+  log: true,
+}
